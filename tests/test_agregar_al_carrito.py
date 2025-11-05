d2b3ef4be8d12b985cd3a31ff7e6ea711f9e1ebb
Pierol <pierol@MacBook-Air-2.local>
2025-11-05 09:10:59 -0500

    fix: Configurar Selenium para modo headless en CI

--- tests/test_agregar_al_carrito.py
+++ tests/test_agregar_al_carrito.py
@@ -25,7 +25,11 @@ def take_screenshot(driver, name):
 def browser():
     """Configura el navegador para las pruebas"""
     options = webdriver.ChromeOptions()
-    options.add_argument("--start-maximized")
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
     options.add_argument("--disable-extensions")
     options.add_argument("--disable-notifications")
     options.add_argument("--disable-infobars")
